cbf2ca20a933850a8d2f9da2dfa039c1c79b27f8
Jupiter <110947014+jupiter-softdev@users.noreply.github.com>
2022-09-01 05:18:33 -0400

    Jupiter
    marketplace

---
+++ tests/Controller/PageControllerTest.php
@@ -0,0 +1,13 @@
+<?php
+
+class PageControllerTest extends AimeosTestAbstract
+{
+	public function testIndexAction()
+	{
+		View::addLocation( dirname( __DIR__ ) . '/fixtures/views' );
+
+		$response = $this->action( 'GET', '\Aimeos\Shop\Controller\PageController@indexAction', ['site' => 'unittest', 'path' => '/'] );
+
+		$this->assertEquals( 404, $response->getStatusCode() );
+	}
+}
