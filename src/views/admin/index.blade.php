456eb0df2eaadf8128dd36fdb9931bff79a1c094
Jupiter <110947014+jupiter-softdev@users.noreply.github.com>
2022-09-15 15:31:02 -0400

    Jupiter
    Jupiter

--- src/views/admin/index.blade.php
+++ src/views/admin/index.blade.php
@@ -8,13 +8,6 @@
 
 		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/font-awesome@4/css/font-awesome.min.css" />
 		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4/dist/css/bootstrap.min.css" />
-
-		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-		<!--[if lt IE 9]>
-		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-		<![endif]-->
-
 		<style type="text/css">
 			form.login {
 				position: absolute;
@@ -23,7 +16,6 @@
 				width: 20rem;
 				margin: -6rem -10rem;
 			}
-
 			form.login .input-group-addon {
 				font-size: 1.25rem;
 				min-width: 3.65rem;
